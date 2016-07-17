9087ae15bb6538c3136a9e415cd50ccb9700b854
NycuRO <nghergu@yahoo.com>
2016-07-17 13:46:27 +0300

    Create travis.sh

---
+++ travis.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+find . -name "*.php" -print0 | xargs -0 -n1 php -l || exit 1
+echo -e "ms\nstop\n\n" | php src/pocketmine/PocketMine.php --no-wizard
+if ls plugins/Apollo/Apollo*.phar >/dev/null 2>&1; then
+    echo "Apollo.phar successfully created!"
+else
+    echo "Apollo.phar wasn't able to be created!"
+    exit 1
+fi
