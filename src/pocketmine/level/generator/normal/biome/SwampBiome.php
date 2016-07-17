5b45ad4054bd23ba6564c7dd3ddfd25808490c01
caspervanneck <nethercube.host@gmail.com>
2016-07-17 16:59:36 +0200

    Add files via upload

---
+++ src/pocketmine/level/generator/normal/biome/SwampBiome.php
@@ -0,0 +1,32 @@
+<?php
+namespace pocketmine\level\generator\normal\biome;
+use pocketmine\block\Block;
+use pocketmine\block\Flower as FlowerBlock;
+use pocketmine\level\generator\populator\Flower;
+use pocketmine\level\generator\populator\LilyPad;
+use pocketmine\level\generator\Populator\Tree;
+class SwampBiome extends GrassyBiome{
+	public function __construct(){
+		parent::__construct();
+
+		$flower = new Flower();
+		$flower->setBaseAmount(8);
+		$flower->addType([Block::RED_FLOWER, FlowerBlock::TYPE_BLUE_ORCHID]);
+		$this->addPopulator($flower);
+		$lilypad = new LilyPad();
+		$lilypad->setBaseAmount(4);
+		$this->addPopulator($lilypad);
+		$trees = new Tree();
+		$trees->setBaseAmount(1);
+		$this->addPopulator($trees);
+		$this->setElevation(62, 63);
+		$this->temperature = 0.8;
+		$this->rainfall = 0.9;
+	}
+	public function getName(){
+		return "Swamp";
+	}
+	public function getColor(){
+		return 0x6a7039;
+	}
+}
