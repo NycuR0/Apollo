2aea4eff9f41ca1406a34cccc941ce3ce19ab6fc
NycuRO <nghergu@yahoo.com>
2016-07-18 20:55:12 +0300

    Update IronDoor.php

--- src/pocketmine/item/IronDoor.php
+++ src/pocketmine/item/IronDoor.php
@@ -25,7 +25,7 @@ use pocketmine\block\Block;
 
 class IronDoor extends Door{
 	public function __construct($meta = 0, $count = 1){
-		$this->block = Block::get(Item::IRON_DOOR_BLOCK);
+		$this->block = Block::get(Item::IRON_DOOR);
 		parent::__construct(self::IRON_DOOR, 0, $count, "Iron Door");
 	}
 }
