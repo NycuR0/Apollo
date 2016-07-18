3201abcf0b77edf84a204bfa37dc1548b9d4afab
NycuRO <nghergu@yahoo.com>
2016-07-18 16:03:32 +0300

    Delete travis.sh

--- travis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-find . -name "*.php" -print0 | xargs -0 -n1 php -l || exit 1
-echo -e "ms\nstop\n\n" | php src/pocketmine/PocketMine.php --no-wizard
-if ls plugins/Apollo/Apollo*.phar >/dev/null 2>&1; then
-    echo "Apollo.phar successfully created!"
-fi
