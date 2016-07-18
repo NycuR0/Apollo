1658b0ffc48d3dc128b3a8273e193ee7caed987b
NycuRO <nghergu@yahoo.com>
2016-07-18 15:46:54 +0300

    Update ServerHandler.php

--- src/raklib/server/ServerHandler.php
+++ src/raklib/server/ServerHandler.php
@@ -53,7 +53,7 @@ class ServerHandler{
 		$this->server->pushMainToThreadPacket($buffer);
 		$this->server->shutdown();
 		$this->server->synchronized(function(){
-			$this->server->wait(20000);
+			//$this->server->wait(20000);
 		});
 		$this->server->join();
 	}
