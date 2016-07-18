03802954943d01bdd7bf7d574a06e406ee28cc39
NycuRO <nghergu@yahoo.com>
2016-07-18 16:11:08 +0300

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -6,8 +6,8 @@ for file in **/*.php; do
     [ $? -ne 0 ] && echo -n "$OUTPUT" && exit 1
 done
 echo Lint done successfully.
-echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Genisys\] Adding "
-if ls plugins/Genisys/Genisys*.phar >/dev/null 2>&1; then
+echo -e "version\nms\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Apollo\] Adding "
+if ls plugins/Apollo/Apollo*.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
     echo No phar created!
