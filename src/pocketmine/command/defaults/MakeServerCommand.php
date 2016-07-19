728252c244fa069a92d372fa8c53574c17cac407
caspervanneck <nethercube.host@gmail.com>
2016-07-19 09:55:21 +0200

    Update MakeServerCommand.php

--- src/pocketmine/command/defaults/MakeServerCommand.php
+++ src/pocketmine/command/defaults/MakeServerCommand.php
@@ -37,7 +37,7 @@ class MakeServerCommand extends VanillaCommand{
 			"geniapi" => $server->getGeniApiVersion(),
 			"minecraft" => $server->getVersion(),
 			"protocol" => Info::CURRENT_PROTOCOL,
-			"creator" => "Genisys MakeServerCommand",
+			"creator" => "Apollo MakeServerCommand",
 			"creationDate" => time()
 		]);
 		$phar->setStub('<?php define("pocketmine\\\\PATH", "phar://". __FILE__ ."/"); require_once("phar://". __FILE__ ."/src/pocketmine/PocketMine.php");  __HALT_COMPILER();');
@@ -52,7 +52,7 @@ class MakeServerCommand extends VanillaCommand{
 				continue;
 			}
 			$phar->addFile($file, $path);
-			$sender->sendMessage("[Genisys] Adding $path");
+			$sender->sendMessage("[Apollo] Adding $path");
 		}
 		foreach($phar as $file => $finfo){
 			/** @var \PharFileInfo $finfo */
