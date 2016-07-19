78e92d2800260ca948bcfd03cf1d5ace2b653a71
caspervanneck <nethercube.host@gmail.com>
2016-07-19 07:32:39 +0200

    Update PocketMine.php

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -72,9 +72,9 @@ namespace pocketmine {
 	use pocketmine\utils\Utils;
 	use pocketmine\wizard\Installer;
 
-	const VERSION = "0.4";
+	const VERSION = "0.9";
 	const API_VERSION = "2.0.0";
-	const CODENAME = "apollo";
+	const CODENAME = "NycuRO";
 	const MINECRAFT_VERSION = "v0.15.0 alpha";
 	const MINECRAFT_VERSION_NETWORK = "0.15.0";
 	const GENISYS_API_VERSION = '1.8.0';
