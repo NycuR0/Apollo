bd1cb3154bc7197932a767babcac05eb5003feaa
caspervanneck <nethercube.host@gmail.com>
2016-07-19 10:28:08 +0200

    Update BatchPacket.php

--- src/pocketmine/network/protocol/BatchPacket.php
+++ src/pocketmine/network/protocol/BatchPacket.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  *
  *  ____            _        _   __  __ _                  __  __ ____  
@@ -17,18 +18,26 @@
  * 
  *
 */
+
 namespace pocketmine\network\protocol;
+
 #include <rules/DataPacket.h>
+
+
 class BatchPacket extends DataPacket{
 	const NETWORK_ID = Info::BATCH_PACKET;
+
 	public $payload;
+
 	public function decode(){
 		$size = $this->getInt();
 		$this->payload = $this->get($size);
 	}
+
 	public function encode(){
 		$this->reset();
 		$this->putInt(strlen($this->payload));
 		$this->put($this->payload);
 	}
+
 }
