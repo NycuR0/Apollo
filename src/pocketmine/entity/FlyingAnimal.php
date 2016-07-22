4fbc25161d16271333eda4c62fd096c96ef02d90
caspervanneck <nethercube.host@gmail.com>
2016-07-22 13:52:39 +0200

    Update FlyingAnimal.php

--- src/pocketmine/entity/FlyingAnimal.php
+++ src/pocketmine/entity/FlyingAnimal.php
@@ -1,128 +1,83 @@
 <?php
 
-/**
- * Author: boybook
- * OpenGenisys Project
- *
- * COPYRIGHT (C) FCCORE
-*/
+namespace pocketmine\entity\;
 
-namespace pocketmine\entity;
-
+use pocketmine\entity\FlyingEntity;
+use pocketmine\entity\Effect;
+use pocketmine\entity\Entity;
 use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Timings;
 use pocketmine\math\Vector3;
+use pocketmine\Player;
 
-abstract class FlyingAnimal extends Creature implements Ageable{
+abstract class FlyingAnimal extends FlyingEntity implements Animal{
 
-    protected $gravity = 0;
-    protected $drag = 0.02;
+    public function getSpeed() : float{
+        return 0.7;
+    }
 
-    /** @var Vector3 */
-    public $flyDirection = null;
-    public $flySpeed = 0.5;
-    public $highestY = 128;
+    public function initEntity(){
+        parent::initEntity();
 
-    private $switchDirectionTicker = 0;
-    public $switchDirectionTicks = 300;
+        if($this->getDataProperty(self::DATA_AGEABLE_FLAGS) === null){
+            $this->setDataProperty(self::DATA_AGEABLE_FLAGS, self::DATA_TYPE_BYTE, 0);
+        }
+    }
+
+    public function isBaby() : bool{
+        return $this->getDataFlag(self::DATA_AGEABLE_FLAGS, self::DATA_FLAG_BABY);
+    }
+
+    public function entityBaseTick($tickDiff = 1){
+        Timings::$timerEntityBaseTick->startTiming();
+
+        $hasUpdate = parent::entityBaseTick($tickDiff);
+
+        if(!$this->hasEffect(Effect::WATER_BREATHING) && $this->isInsideOfWater()){
+            $hasUpdate = true;
+            $airTicks = $this->getDataProperty(self::DATA_AIR) - $tickDiff;
+            if($airTicks <= -20){
+                $airTicks = 0;
+                $ev = new EntityDamageEvent($this, EntityDamageEvent::CAUSE_DROWNING, 2);
+                $this->attack($ev->getFinalDamage(), $ev);
+            }
+            $this->setDataProperty(Entity::DATA_AIR, Entity::DATA_TYPE_SHORT, $airTicks);
+        }else{
+            $this->setDataProperty(Entity::DATA_AIR, Entity::DATA_TYPE_SHORT, 300);
+        }
+
+        Timings::$timerEntityBaseTick->stopTiming();
+        return $hasUpdate;
+    }
 
     public function onUpdate($currentTick){
-        if($this->closed !== false){
-            return false;
-        }
-        if ($this->willMove(100) and $this->getLevel()->getServer()->aiEnabled) {
-            if(++$this->switchDirectionTicker === $this->switchDirectionTicks){
-                $this->switchDirectionTicker = 0;
-                if(mt_rand(0, 100) < 50){
-                    $this->flyDirection = null;
-                }
+        if(!$this->isAlive()){
+            if(++$this->deadTicks >= 23){
+                $this->close();
+                return false;
             }
+            return true;
+        }
 
-            $this->lastUpdate = $currentTick;
+        $tickDiff = $currentTick - $this->lastUpdate;
+        $this->lastUpdate = $currentTick;
+        $this->entityBaseTick($tickDiff);
 
-            $this->timings->startTiming();
-
-            if($this->isAlive()){
-
-                if($this->y > $this->highestY and $this->flyDirection !== null){
-                    $this->flyDirection->y = -0.5;
-                }
-
-                $inAir = !$this->isInsideOfSolid() and !$this->isInsideOfWater();
-                if(!$inAir){
-                    $this->flyDirection = null;
-                }
-                if($this->flyDirection instanceof Vector3){
-                    //var_dump($this->flyDirection);
-                    $this->setMotion($this->flyDirection->multiply($this->flySpeed));
-                }else{
-                    $this->flyDirection = $this->generateRandomDirection();
-                    $this->flySpeed = mt_rand(50, 100) / 500;
-                    $this->setMotion($this->flyDirection);
-                }
-
-                //$expectedPos = new Vector3($this->x + $this->motionX, $this->y + $this->motionY, $this->z + $this->motionZ);
-
-                //$motion = $this->flyDirection->multiply($this->flySpeed);
-                $this->move($this->motionX, $this->motionY, $this->motionZ);
-                $this->updateMovement();
-                //$this->getLevel()->addEntityMotion($this->chunk->getX(), $this->chunk->getZ(), $this->getId(), $motion->x, $motion->y, $motion->z);
-
-                //echo "EID = {$this->getId()}, motionX = $this->motionX, motionY = $this->motionY, motionZ = $this->motionZ\n";
-                /*
-
-                if($expectedPos->distanceSquared($this) > 0){
-                    $this->flyDirection = $this->generateRandomDirection();
-                    $this->flySpeed = mt_rand(50, 100) / 500;
-                }
-
-                $friction = 1 - $this->drag;
-
-                $this->motionX *= $friction;
-                $this->motionY *= 1 - $this->drag;
-                $this->motionZ *= $friction;
-*/
-                $f = sqrt(($this->motionX ** 2) + ($this->motionZ ** 2));
-                $this->yaw = (-atan2($this->motionX, $this->motionZ) * 180 / M_PI);
-                $this->pitch = (-atan2($f, $this->motionY) * 180 / M_PI);
-
-                if($this->onGround and $this->flyDirection instanceof Vector3){
-                    $this->flyDirection->y *= -1;
-                }
-
-
+        $target = $this->updateMove($tickDiff);
+        if($target instanceof Player){
+            if($this->distance($target) <= 2){
+                $this->pitch = 22;
+                $this->x = $this->lastX;
+                $this->y = $this->lastY;
+                $this->z = $this->lastZ;
             }
+        }elseif(
+            $target instanceof Vector3
+            && $this->distance($target) <= 1
+        ){
+            $this->moveTime = 0;
         }
-        parent::onUpdate($currentTick);
-        //parent::entityBaseTick();
-        $this->timings->stopTiming();
-
-        return !$this->onGround or abs($this->motionX) > 0.00001 or abs($this->motionY) > 0.00001 or abs($this->motionZ) > 0.00001;
-    }
-
-    private function generateRandomDirection(){
-        return new Vector3(mt_rand(-1000, 1000) / 1000, mt_rand(-500, 500) / 1000, mt_rand(-1000, 1000) / 1000);
-    }
-
-	public function initEntity(){
-		parent::initEntity();
-		if($this->getDataProperty(self::DATA_AGEABLE_FLAGS) === null){
-			$this->setDataProperty(self::DATA_AGEABLE_FLAGS, self::DATA_TYPE_BYTE, 0);
-		}
-	}
-
-	public function isBaby(){
-		return $this->getDataFlag(self::DATA_AGEABLE_FLAGS, self::DATA_FLAG_BABY);
-	}
-
-    public function attack($damage, EntityDamageEvent $source){
-        if($source->isCancelled()){
-            return;
-        }
-        if ($source->getCause() == EntityDamageEvent::CAUSE_FALL) {
-            $source->setCancelled();
-            return;
-        }
-        parent::attack($damage, $source);
+        return true;
     }
 
 }
