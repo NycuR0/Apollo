59b3116f239dcb25accb0c51b8934779a94052f2
NycuRO <nghergu@yahoo.com>
2016-07-28 22:24:05 +0300

    Update TallGrass.php

--- src/pocketmine/level/generator/object/TallGrass.php
+++ src/pocketmine/level/generator/object/TallGrass.php
@@ -25,8 +25,9 @@ use pocketmine\block\Block;
 use pocketmine\level\ChunkManager;
 use pocketmine\math\Vector3 as Vector3;
 use pocketmine\utils\Random;
+use pocketmine\level\generator\object\Object;
 
-class TallGrass{
+class TallGrass extends Object{
 	public static function growGrass(ChunkManager $level, Vector3 $pos, Random $random, $count = 15, $radius = 10){
 		$arr = [
 			[Block::DANDELION, 0],
