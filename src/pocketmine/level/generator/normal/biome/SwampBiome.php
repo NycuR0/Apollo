8ebfacccaa539c54b2ea350b0315bf8332f2ea73
NycuRO <nghergu@yahoo.com>
2016-07-28 20:34:19 +0300

    Update SwampBiome.php

--- src/pocketmine/level/generator/normal/biome/SwampBiome.php
+++ src/pocketmine/level/generator/normal/biome/SwampBiome.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Finish
+Finish,without need add mushroom
 */
 
 namespace pocketmine\level\generator\normal\biome;
@@ -11,6 +11,8 @@ use pocketmine\block\Flower as FlowerBlock;
 use pocketmine\level\generator\populator\Flower;
 use pocketmine\level\generator\populator\LilyPad;
 use pocketmine\level\generator\populator\Tree;
+use pocketmine\level\generator\normal\populator\SugarCane;
+use pocketmine\level\generator\normal\populator\TallGrass;
 
 class SwampBiome extends GrassyBiome{
 
@@ -26,11 +28,20 @@ class SwampBiome extends GrassyBiome{
 		$lilypad->setBaseAmount(4);
 		$this->addPopulator($lilypad);
 		
+		$tallGrass = new TallGrass();
+		$tallGrass->setBaseAmount(1);
+		
+		$sugarCane = new SugarCane();
+		$sugarCane->setBaseAmount(2);
+		$sugarCane->setRandomAmount(15);
+		
 		$trees = new Tree();
 		$trees->setBaseAmount(3);
 		$this->addPopulator($trees);
+		$this->addPopulator($sugarCane);
+		$this->addPopulator($tallGrass);
 
-		$this->setElevation(62, 63);
+		$this->setElevation(60, 66);
 
 		$this->temperature = 0.80;
 		$this->rainfall = 0.90;
