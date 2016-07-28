c445d743dd0337506eb77ecfb8911d9453e7ee04
NycuRO <nghergu@yahoo.com>
2016-07-28 23:20:50 +0300

    Update OceanBiome.php

--- src/pocketmine/level/generator/normal/biome/OceanBiome.php
+++ src/pocketmine/level/generator/normal/biome/OceanBiome.php
@@ -15,9 +15,7 @@ class OceanBiome extends WateryBiome{
 		parent::__construct();
 
 		$sugarcane = new SugarCane();
-		$sugarcane->setBaseAmount(6);
 		$tallGrass = new TallGrass();
-		$tallGrass->setBaseAmount(5);
 
 		$this->addPopulator($sugarcane);
 		$this->addPopulator($tallGrass);
