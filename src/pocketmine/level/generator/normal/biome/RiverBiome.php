05853b02a387326c6588ede610298fc036fcfabf
NycuRO <nghergu@yahoo.com>
2016-07-31 14:21:03 +0300

    Update RiverBiome.php

--- src/pocketmine/level/generator/normal/biome/RiverBiome.php
+++ src/pocketmine/level/generator/normal/biome/RiverBiome.php
@@ -1,31 +1,24 @@
 <?php
-
 /*
 Finish
 */
-
 namespace pocketmine\level\generator\normal\biome;
-
 use pocketmine\level\generator\populator\SugarCane;
 use pocketmine\level\generator\populator\TallGrass;
 use pocketmine\block\Block;
 class RiverBiome extends WateryBiome{
-
 	public function __construct(){
 		parent::__construct();
-
 		$sugarcane = new SugarCane();
+		$sugarcane->setBaseAmount(6);
 		$tallGrass = new TallGrass();
-
+		$tallGrass->setBaseAmount(25);
 		$this->addPopulator($sugarcane);
 		$this->addPopulator($tallGrass);
-
 		$this->setElevation(58, 62);
-
 		$this->temperature = 0.50;
 		$this->rainfall = 0.70;
 	}
-
 	public function getName() : string{
 		return "River";
 	}
