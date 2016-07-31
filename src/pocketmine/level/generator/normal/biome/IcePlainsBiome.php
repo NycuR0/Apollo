85c35596de25458740a1b86973b594d4ccf2798e
NycuRO <nghergu@yahoo.com>
2016-07-31 14:14:30 +0300

    Update IcePlainsBiome.php

--- src/pocketmine/level/generator/normal/biome/IcePlainsBiome.php
+++ src/pocketmine/level/generator/normal/biome/IcePlainsBiome.php
@@ -1,33 +1,24 @@
 <?php
-
 /*
 Finish
 */
-
 namespace pocketmine\level\generator\normal\biome;
-
 use pocketmine\level\generator\populator\TallGrass;
 use pocketmine\level\generator\populator\Tree;
 use pocketmine\block\Sapling;
-
 class IcePlainsBiome extends SnowyBiome{
-
 	public function __construct(){
 		parent::__construct();
-
 		$tallGrass = new TallGrass();
-
+		$tallGrass->setBaseAmount(5);
 		$trees = new Tree(Sapling::SPRUCE);
+		$trees->setBaseAmount(1);
 		$this->addPopulator($trees);
-
 		$this->addPopulator($tallGrass);
-
 		$this->setElevation(63, 74);
-
 		$this->temperature = 0.05;
 		$this->rainfall = 0.80;
 	}
-
 	public function getName() : string{
 		return "Ice Plains";
 	}
