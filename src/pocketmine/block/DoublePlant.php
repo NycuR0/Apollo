747f12ad860c390d9f2de9682b98d98d58b9c557
NycuRO <nghergu@yahoo.com>
2016-07-31 16:21:15 +0300

    Update DoublePlant.php

--- src/pocketmine/block/DoublePlant.php
+++ src/pocketmine/block/DoublePlant.php
@@ -1,12 +1,32 @@
 <?php
+
 /*
-From Nukkit and Genisys. Thanks
+ *
+ *  ____            _        _   __  __ _                  __  __ ____  
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \ 
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/ 
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_| 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ * 
+ *
 */
+
 namespace pocketmine\block;
+
 use pocketmine\item\Item;
 use pocketmine\level\Level;
 use pocketmine\Player;
+
 class DoublePlant extends Flowable{
+
 	protected $id = self::DOUBLE_PLANT;
 	
 	const SUNFLOWER = 0;
@@ -15,15 +35,15 @@ class DoublePlant extends Flowable{
 	const LARGE_FERN = 3;
 	const ROSE_BUSH = 4;
 	const PEONY = 5;
+
 	public function __construct($meta = 0){
 		$this->meta = $meta;
 	}
-        public function DoublePlant(int $meta){
-                super($meta);
-        }
+
 	public function canBeReplaced(){
-		return $this->meta == 2 || $this->meta == 3;
+		return true;
 	}
+
 	public function getName() : string{
 		static $names = [
 			0 => "Sunflower",
@@ -35,24 +55,19 @@ class DoublePlant extends Flowable{
 		];
 		return $names[$this->meta & 0x07];
 	}
+
 	public function onUpdate($type){
 		if($type === Level::BLOCK_UPDATE_NORMAL){
-			if(($this->meta & 0x08) == 8){
-				//top
-				if(!($this->getSide(0) instanceof DoublePlant)){
-                                   $this->getLevel()->setBlock($this, new Air(), false, false);
-                                   return Level::BLOCK_UPDATE_NORMAL;
-                                }
-			//botom
-				if($this->getSide(0)->isTransparent() || !$this->getSide(1) instanceof DoublePlant){ //Replace with common break method
-                                   $this->getLevel()->setBlock($this, new Air(), false, false);
-                                   return Level::BLOCK_UPDATE_NORMAL;
-                                }
+			if($this->getSide(0)->isTransparent() || true && !$this->getSide(1) instanceof DoublePlant){ //Replace with common break method
+				$this->getLevel()->setBlock($this, new Air(), false, false);
+				return Level::BLOCK_UPDATE_NORMAL;
 			}
 		}
-		return 0;
+
+		return false;
 	}
-	/*public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
+
+	public function place(Item $item, Block $block, Block $target, $face, $fx, $fy, $fz, Player $player = null){
 		$down = $this->getSide(0);
 		$up = $this->getSide(1);
 		if($down->getId() === self::GRASS or $down->getId() === self::DIRT){
@@ -62,10 +77,30 @@ class DoublePlant extends Flowable{
 		}
 		return false;
 	}
-	public function getDrops(Item $item){
-		return false;
-	}*/
-        public function getColor(){
-                return BlockColor::FOLIAGE_BLOCK_COLOR;
-        }
+
+	public function onBreak(Item $item){
+		$up = $this->getSide(1);
+		$down = $this->getSide(0);
+		if(($this->meta & 0x08) === 0x08){ // This is the Top part of flower
+			if($up->getId() === $this->id and $up->meta !== 0x08){ // Checks if the block ID and meta are right
+				$this->getLevel()->setBlock($up, new Air(), true, true);
+			}elseif($down->getId() === $this->id and $down->meta !== 0x08){
+				$this->getLevel()->setBlock($down, new Air(), true, true);
+			}
+		}else{ // Bottom Part of flower
+			if($up->getId() === $this->id and ($up->meta & 0x08) === 0x08){
+				$this->getLevel()->setBlock($up, new Air(), true, true);
+			}elseif($down->getId() === $this->id and ($down->meta & 0x08) === 0x08){
+				$this->getLevel()->setBlock($down, new Air(), true, true);
+			}
+		}
+	}
+
+	public function getDrops(Item $item) : array{
+		if(($this->meta & 0x08) !== 0x08){
+			return [[Item::DOUBLE_PLANT, $this->meta, 1]];
+		}else{
+			return [];
+		}
+	}
 }
