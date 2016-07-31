09526009e3e9c2c414af114beab2829af2e33333
NycuRO <nghergu@yahoo.com>
2016-07-31 14:09:25 +0300

    Update ForestBiome.php

--- src/pocketmine/level/generator/normal/biome/ForestBiome.php
+++ src/pocketmine/level/generator/normal/biome/ForestBiome.php
@@ -1,9 +1,7 @@
 <?php
-
 /*
 Finish
 */
-
 namespace pocketmine\level\generator\normal\biome;
 use pocketmine\block\Block;
 use pocketmine\block\Sapling;
@@ -11,35 +9,32 @@ use pocketmine\level\generator\populator\TallGrass;
 use pocketmine\level\generator\populator\Tree;
 use pocketmine\level\generator\populator\Flower;
 use pocketmine\level\generator\populator\Pumpkin;
-
 class ForestBiome extends GrassyBiome{
-
 	const TYPE_NORMAL = 0;
 	const TYPE_BIRCH = 1;
-
 	public $type;
-
 	public function __construct($type = self::TYPE_NORMAL){
 		parent::__construct();
-
 		$this->type = $type;
-
 		$trees = new Tree($type === self::TYPE_BIRCH ? Sapling::BIRCH : Sapling::OAK);
+		$trees->setBaseAmount(5);
 		$this->addPopulator($trees);
 		
 		$flower = new Flower();
+		$flower->setBaseAmount(0);
+		$flower->setRandomAmount(5);
 		$flower->addType([Block::DANDELION, 0]);
 		$this->addPopulator($flower);
-
 		$tallGrass = new TallGrass();
-
+		$tallGrass->setBaseAmount(5);
+		$tallGrass->setRandomAmount(5);
 		$this->addPopulator($tallGrass);
 		
 		$pumpkin = new Pumpkin();
+		$pumpkin->setBaseAmount(0);
+		$pumpkin->setRandomAmount(1);
 		$this->addPopulator($pumpkin);
-
 		$this->setElevation(63, 68);
-
 		if($type === self::TYPE_BIRCH){
 			$this->temperature = 0.60;
 			$this->rainfall = 0.50;
@@ -48,12 +43,10 @@ class ForestBiome extends GrassyBiome{
 			$this->rainfall = 0.50;
 		}
 	}
-
 	public function getName() : string{
 		return $this->type === self::TYPE_BIRCH ? "Birch Forest" : "Forest";
 	}
 	public function getColor(){
 		return 0x8CBB5F;
 	}
-
 }
