4b6c978010c6e4b8ad6ea5bdf88d4bf084af3a27
NycuRO <nghergu@yahoo.com>
2016-07-31 14:23:36 +0300

    Update SandyBiome.php

--- src/pocketmine/level/generator/normal/biome/SandyBiome.php
+++ src/pocketmine/level/generator/normal/biome/SandyBiome.php
@@ -1,30 +1,26 @@
 <?php
-
 /*
 Finish. Principal biome of cold biomes.
 */
-
 namespace pocketmine\level\generator\normal\biome;
-
 use pocketmine\block\Block;
 use pocketmine\level\generator\populator\Cactus;
 use pocketmine\level\generator\populator\DeadBush;
 use pocketmine\level\generator\populator\Sugarcane;
-
 class SandyBiome extends GrassyBiome{
-
 	public function __construct(){
 		parent::__construct();
-
 		$cactus = new Cactus();
+		$cactus->setBaseAmount(2);
+		$cactus->setRandomAmount(1);
 		$deadBush = new DeadBush();
+		$deadBush->setBaseAmount(1);
 		$sugarcane = new Sugarcane();
+		$sugarcane->setBaseAmount(6);
                 $this->addPopulator($sugarcane);
 		$this->addPopulator($cactus);
 		$this->addPopulator($deadBush);
-
 		$this->setElevation(63, 81);
-
 		$this->temperature = 0.05;
 		$this->rainfall = 0.80;
 		$this->setGroundCover([
@@ -39,7 +35,6 @@ class SandyBiome extends GrassyBiome{
 			Block::get(Block::SANDSTONE, 0),
 		]);
 	}
-
 	public function getName() : string{
 		return "Sandy";
 	}
