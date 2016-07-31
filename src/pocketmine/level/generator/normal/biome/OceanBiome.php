c853ef69de89881df9cefb630867a4fc92dff8fe
NycuRO <nghergu@yahoo.com>
2016-07-31 14:19:05 +0300

    Update OceanBiome.php

--- src/pocketmine/level/generator/normal/biome/OceanBiome.php
+++ src/pocketmine/level/generator/normal/biome/OceanBiome.php
@@ -1,31 +1,23 @@
 <?php
-
 /*
 Need change. Islands is biomes ,no need add populators here
 */
-
 namespace pocketmine\level\generator\normal\biome;
-
 use pocketmine\level\generator\populator\SugarCane;
 use pocketmine\level\generator\populator\TallGrass;
-
 class OceanBiome extends WateryBiome{
-
 	public function __construct(){
 		parent::__construct();
-
 		$sugarcane = new SugarCane();
+		$sugarcane->setBaseAmount(6);
 		$tallGrass = new TallGrass();
-
+		$tallGrass->setBaseAmount(5);
 		$this->addPopulator($sugarcane);
 		$this->addPopulator($tallGrass);
-
 		$this->setElevation(46, 68);
-
 		$this->temperature = 0.50;
 		$this->rainfall = 0.50;
 	}
-
 	public function getName() : string{
 		return "Ocean";
 	}
