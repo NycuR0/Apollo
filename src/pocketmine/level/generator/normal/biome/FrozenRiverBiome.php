522e9b3a209a198b24e859cd75b7fc5e848ecf32
NycuRO <nghergu@yahoo.com>
2016-07-31 14:11:33 +0300

    Update FrozenRiverBiome.php

--- src/pocketmine/level/generator/normal/biome/FrozenRiverBiome.php
+++ src/pocketmine/level/generator/normal/biome/FrozenRiverBiome.php
@@ -2,7 +2,6 @@
 /*
 Finish ,more in future
 */
-
 namespace pocketmine\level\generator\normal\biome;
 use pocketmine\block\Block;
 use pocketmine\level\generator\populator\tree;
@@ -10,8 +9,8 @@ use pocketmine\block\Sapling;
 class FrozenRiverBiome extends SnowyBiome{
 	public function __construct(){
 		parent::__construct();
-
 		$trees = new Tree(Sapling::SPRUCE);
+		$trees->setRandomAmount(1);
 		$this->addPopulator($trees);
 		
 		$this->temperature = 0.0;
