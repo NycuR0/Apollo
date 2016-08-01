74889db1841e6c402082aa36a686ed448b55af0c
caspervanneck <nethercube.host@gmail.com>
2016-08-01 12:23:22 +0200

    try to fix Server Could not execute asynchronous task PopulationTask: Task crashedead/CRITICAL]:

--- src/pocketmine/level/generator/populator/Cactus.php
+++ src/pocketmine/level/generator/populator/Cactus.php
@@ -24,33 +24,55 @@ namespace pocketmine\level\generator\populator;
 use pocketmine\block\Block;
 use pocketmine\level\ChunkManager;
 use pocketmine\utils\Random;
-use pocketmine\level\generator\object\CactusStack;
-use pocketmine\level\generator\populator\VariableAmountPopulator;
 
-class Cactus extends VariableAmountPopulator{
+class Cactus extends Populator{
+	/** @var ChunkManager */
+	private $level;
+	private $randomAmount;
+	private $baseAmount;
 
-	public function __construct(){
-		parent::__construct(2, 1);
+	public function setRandomAmount($amount){
+		$this->randomAmount = $amount;
+	}
+
+	public function setBaseAmount($amount){
+		$this->baseAmount = $amount;
 	}
 
 	public function populate(ChunkManager $level, $chunkX, $chunkZ, Random $random){
-		$amount = $this->getAmount($random);
-		$cactus = new CactusStack($random);
+		$this->level = $level;
+		$amount = $random->nextRange(0, $this->randomAmount + 1) + $this->baseAmount;
 		for($i = 0; $i < $amount; ++$i){
 			$x = $random->nextRange($chunkX * 16, $chunkX * 16 + 15);
 			$z = $random->nextRange($chunkZ * 16, $chunkZ * 16 + 15);
 			$y = $this->getHighestWorkableBlock($x, $z);
-			$cactus->randomize();
-			if($y !== -1 and $cactus->canPlaceObject($level, $x, $y, $z)){
-				$cactus->placeObject($level, $x, $y, $z);
+			$tallRand = $random->nextRange(0, 17);
+			$yMax = $y + 1 + (int) ($tallRand > 10) + (int) ($tallRand > 15);
+
+			if($y !== -1){
+				for(; $y < 127 and $y < $yMax; $y++){
+					if($this->canCactusStay($x, $y, $z)){
+						$this->level->setBlockIdAt($x, $y, $z, Block::CACTUS);
+						$this->level->setBlockDataAt($x, $y, $z, 1);
+					}
+				}
 			}
 		}
+	}
+
+	private function canCactusStay($x, $y, $z){
+		$b = $this->level->getBlockIdAt($x, $y, $z);
+		$below = $this->level->getBlockIdAt($x, $y - 1, $z);
+		foreach(array($this->level->getBlockIdAt($x + 1, $y, $z), $this->level->getBlockIdAt($x - 1, $y, $z), $this->level->getBlockIdAt($x, $y, $z + 1), $this->level->getBlockIdAt($x, $y, $z - 1)) as $adjacent){
+			if($adjacent !== Block::AIR) return false;
+		}
+		return ($b === Block::AIR) and ($below === Block::SAND or $below === Block::CACTUS);
 	}
 
 	private function getHighestWorkableBlock($x, $z){
 		for($y = 127; $y >= 0; --$y){
 			$b = $this->level->getBlockIdAt($x, $y, $z);
-			if($b !== Block::SAND and $b !== Block::LEAVES and $b !== Block::LEAVES2){
+			if($b !== Block::AIR and $b !== Block::LEAVES and $b !== Block::LEAVES2 and $b !== Block::SNOW_LAYER){
 				break;
 			}
 		}
