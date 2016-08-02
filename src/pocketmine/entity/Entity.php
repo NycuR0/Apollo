abc1fa9686f104a844ddf431d07b8da504037787
caspervanneck <nethercube.host@gmail.com>
2016-08-02 13:19:41 +0200

    TEST

--- src/pocketmine/entity/Entity.php
+++ src/pocketmine/entity/Entity.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- *
- *
-*/
-
 /**
  * All the entity classes
  */
@@ -80,8 +61,8 @@ abstract class Entity extends Location implements Metadatable{
 	const DATA_TYPE_STRING = 4;
 	const DATA_TYPE_SLOT = 5;
 	const DATA_TYPE_POS = 6;
-	//const DATA_TYPE_ROTATION = 8;
-	//const DATA_TYPE_LONG = 8;
+        const DATA_TYPE_ROTATION = 8;
+	const DATA_TYPE_LONG = 8;
 	const DATA_TYPE_LONG = 7;
 	
 	const DATA_FLAGS = 0;
