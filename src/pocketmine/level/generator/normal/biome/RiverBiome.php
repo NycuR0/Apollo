b5bbc87535fd266016e77d0a10a26cafbb3f712c
NycuRO <nghergu@yahoo.com>
2016-08-08 15:58:54 +0300

    Update RiverBiome.php

--- src/pocketmine/level/generator/normal/biome/RiverBiome.php
+++ src/pocketmine/level/generator/normal/biome/RiverBiome.php
@@ -5,6 +5,7 @@ Finish
 namespace pocketmine\level\generator\normal\biome;
 use pocketmine\level\generator\populator\SugarCane;
 use pocketmine\level\generator\populator\TallGrass;
+use pocketmine\level\generator\populator\Mushroom;
 use pocketmine\block\Block;
 class RiverBiome extends WateryBiome{
 	public function __construct(){
@@ -13,6 +14,8 @@ class RiverBiome extends WateryBiome{
 		$sugarcane->setBaseAmount(6);
 		$tallGrass = new TallGrass();
 		$tallGrass->setBaseAmount(25);
+		$mushroom = new Mushroom();
+		$this->addPopulator($mushroom);
 		$this->addPopulator($sugarcane);
 		$this->addPopulator($tallGrass);
 		$this->setElevation(58, 62);
