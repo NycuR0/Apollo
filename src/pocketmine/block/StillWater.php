da5fb682edb8ef6a4d4382163c93a5a74f847e2c
NycuRO <nghergu@yahoo.com>
2016-08-11 17:42:03 +0300

    Revert "Add files via upload"
    This reverts commit 7652d17d7aaf21382a13ed1e391efe670f843804.

--- src/pocketmine/block/StillWater.php
+++ src/pocketmine/block/StillWater.php
@@ -21,19 +21,11 @@
 
 namespace pocketmine\block;
 
-use pocketmine\level\Level;
-
 class StillWater extends Water{
 
 	protected $id = self::STILL_WATER;
 
-	public function onUpdate($type){
-		if($type == Level::BLOCK_UPDATE_NORMAL){
-			parent::onUpdate($type);
-		}
-	}
-
-	public function getName() : string{
+	public function getName(){
 		return "Still Water";
 	}
 }
