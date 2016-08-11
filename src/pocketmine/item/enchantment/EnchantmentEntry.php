2dfa00eca2816cd7b5607edf5553152ebe8f2494
NycuRO <nghergu@yahoo.com>
2016-08-11 16:34:06 +0300

    Update EnchantmentEntry.php

--- src/pocketmine/item/enchantment/EnchantmentEntry.php
+++ src/pocketmine/item/enchantment/EnchantmentEntry.php
@@ -21,19 +21,19 @@
 
 namespace pocketmine\item\enchantment;
 
-
 class EnchantmentEntry{
-
 	/** @var Enchantment[] */
 	private $enchantments;
 	private $cost;
 	private $randomName;
 
 	/**
-	 * @param Enchantment[] $enchantments
-	 * @param $cost
-	 * @param $randomName
-	 */
+	 * @param Enchantment[] $enchantments 
+	 * @param
+	 * $cost
+	 * @param
+	 * $randomName
+  	 */
 	public function __construct(array $enchantments, $cost, $randomName){
 		$this->enchantments = $enchantments;
 		$this->cost = (int) $cost;
