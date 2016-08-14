a0f800e144e2b341814306471e41987c4155f7ec
NycuRO <nghergu@yahoo.com>
2016-08-14 22:09:55 +0300

    Update SayCommand.php

--- src/pocketmine/command/defaults/SayCommand.php
+++ src/pocketmine/command/defaults/SayCommand.php
@@ -50,7 +50,7 @@ class SayCommand extends VanillaCommand{
 			return false;
 		}
 
-		$sender->getServer()->broadcastMessage(new TranslationContainer(TextFormat::YELLOW . "%chat.type.announcement", $sender instanceof Player ? $sender->getDisplayName() : ($sender instanceof ConsoleCommandSender ? "Server" : $sender->getName()), TextFormat::LIGHT_GRAY . implode(" »", $args)));
+		$sender->getServer()->broadcastMessage(new TranslationContainer(TextFormat::YELLOW . "%chat.type.announcement", $sender instanceof Player ? $sender->getDisplayName() : ($sender instanceof ConsoleCommandSender ? "Server" : $sender->getName()), TextFormat::GRAY . implode(" »", $args)));
 		return true;
 	}
 }
