f98280a71adc6688f41456e49791a35338a381fb
NycuRO <nghergu@yahoo.com>
2016-08-14 22:39:44 +0300

    Update SayCommand.php

--- src/pocketmine/command/defaults/SayCommand.php
+++ src/pocketmine/command/defaults/SayCommand.php
@@ -38,7 +38,7 @@ class SayCommand extends VanillaCommand{
 		$this->setPermission("pocketmine.command.say");
 	}
 
-	public function execute(CommandSender $sender, $currentAlias, array $args){c
+	public function execute(CommandSender $sender, $currentAlias, array $args){
 		if(!$this->testPermission($sender)){
 			return true;
 		}
