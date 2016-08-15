4256b0c1fba6366c0391ad80c8b5d8547fb0296d
caspervanneck <nethercube.host@gmail.com>
2016-08-15 17:27:47 +0200

    Update loop.cmd

--- loop.cmd
+++ loop.cmd
@@ -1,32 +1,20 @@
 @echo off
-TITLE Apollo: Auto restart
 cd /d %~dp0
-netstat -o -n -a | findstr 0.0.0.0:19132>nul
+netstat -o -n -a | findstr 0.0.0.0:19132 > NUL
 if %ERRORLEVEL% equ 0 (
-    echo Your server is running.
     goto :loop
-) ELSE (
-    echo Starting your PocketMine-MP server.
+) else (
     goto :StartPM
 )
-
-
 :loop
-echo Checking if server is online...
-PING 127.0.0.1 -n 10 >NUL
-
-
-netstat -o -n -a | findstr 0.0:19132>nul
+ping 127.0.0.1 -n 5 > NUL
+netstat -o -n -a | findstr 0.0:19132 > NUL
 if %ERRORLEVEL% equ 0 (
-    echo Server is running.
     goto :loop
-) ELSE (
-    echo Starting PocketMine-MP in 10 seconds...
-    PING 127.0.0.1 -n 10 >NUL
+) else (
+    PING 127.0.0.1 -n 5 > NUL
     goto :StartPM
 )
-
-
 :StartPM
 if exist bin\php\php.exe (
     set PHP_BINARY=bin\php\php.exe
@@ -40,19 +28,18 @@ if exist Apollo*.phar (
     if exist src\pocketmine\PocketMine.php (
         set POCKETMINE_FILE=src\pocketmine\PocketMine.php
     ) else (
-        echo "Couldn't find a valid PocketMine-MP installation"
+        echo "Couldn't find a valid Apollo installation"
         pause
         exit 1
+    )
 )
-)
-
-#if exist bin\php\php_wxwidgets.dll (
-#    %PHP_BINARY% %POCKETMINE_FILE% --enable-gui %*
-#) else (
+if exist bin\php\php_wxwidgets.dll (
+    %PHP_BINARY% %POCKETMINE_FILE% --enable-gui %*
+) else (
     if exist bin\mintty.exe (
         start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
     ) else (
         %PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
     )
-#)
+)
 goto :loop
