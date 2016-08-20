8d24bdce02dc0f66e6f1b0f93a35aac85e2ab9ec
NycuRO <nghergu@yahoo.com>
2016-08-21 00:30:32 +0300

    Update PlayerLogoutPacket.php

--- src/synapse/network/protocol/spp/PlayerLogoutPacket.php
+++ src/synapse/network/protocol/spp/PlayerLogoutPacket.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  _____   _____   __   _   _   _____  __    __  _____
@@ -20,22 +19,18 @@
  */
  
 namespace synapse\network\protocol\spp;
-
 use pocketmine\utils\UUID;
-
 class PlayerLogoutPacket extends DataPacket{
 	const NETWORK_ID = Info::PLAYER_LOGOUT_PACKET;
 	
 	/** @var UUID */
 	public $uuid;
 	public $reason;
-
 	public function encode(){
 		$this->reset();
 		$this->putUUID($this->uuid);
 		$this->putString($this->reason);
 	}
-
 	public function decode(){
 		$this->uuid = $this->getUUID();
 		$this->reason = $this->getString();
