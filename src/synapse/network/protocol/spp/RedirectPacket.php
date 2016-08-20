cf38a9d3c406390bb0ce8f3fc14717e8c359bd93
NycuRO <nghergu@yahoo.com>
2016-08-21 00:30:52 +0300

    Update RedirectPacket.php

--- src/synapse/network/protocol/spp/RedirectPacket.php
+++ src/synapse/network/protocol/spp/RedirectPacket.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  *
  *  _____   _____   __   _   _   _____  __    __  _____
@@ -20,24 +19,19 @@
  */
  
 namespace synapse\network\protocol\spp;
-
 use pocketmine\utils\UUID;
-
 class RedirectPacket extends DataPacket{
 	const NETWORK_ID = Info::REDIRECT_PACKET;
-
 	/** @var UUID */
 	public $uuid;
 	public $direct;
 	public $mcpeBuffer;
-
 	public function encode(){
 		$this->reset();
 		$this->putUUID($this->uuid);
 		$this->putByte($this->direct ? 1 : 0);
 		$this->putString($this->mcpeBuffer);
 	}
-
 	public function decode(){
 		$this->uuid = $this->getUUID();
 		$this->direct = ($this->getByte() == 1) ? true : false;
