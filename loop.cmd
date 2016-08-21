cad11f8b69f07d61a41b58a47c42b58a964c201b
caspervanneck <nethercube.host@gmail.com>
2016-08-21 10:27:40 +0200

    Update loop.cmd

--- loop.cmd
+++ loop.cmd
@@ -33,13 +33,11 @@ if exist Apollo*.phar (
         exit 1
     )
 )
-if exist bin\php\php_wxwidgets.dll (
-    %PHP_BINARY% %POCKETMINE_FILE% --enable-gui %*
-) else (
-    if exist bin\mintty.exe (
-        start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="DejaVu Sans Mono" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "PocketMine-MP" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
-    ) else (
-        %PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
-    )
-)
+   REM 	%PHP_BINARY% %POCKETMINE_FILE% --enable-gui %*
+REM ) else (
+	if exist bin\mintty.exe (
+		start "" bin\mintty.exe -o Columns=88 -o Rows=32 -o AllowBlinking=0 -o FontQuality=3 -o Font="Consolas" -o FontHeight=10 -o CursorType=0 -o CursorBlinks=1 -h error -t "ClearSky" -i bin/pocketmine.ico -w max %PHP_BINARY% %POCKETMINE_FILE% --enable-ansi %*
+	) else (
+		%PHP_BINARY% -c bin\php %POCKETMINE_FILE% %*
+	)
 goto :loop
