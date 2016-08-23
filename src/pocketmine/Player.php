1ef91ee86571a25705a1f069138340d18728fde9
NycuRO <nghergu@yahoo.com>
2016-08-23 17:51:39 +0300

    Update Player.php

--- src/pocketmine/Player.php
+++ src/pocketmine/Player.php
@@ -244,11 +244,6 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 	/** @var Vector3 */
 	protected $sleeping = null;
 	protected $clientID = null;
-	
-	//Assume client is desktop until told otherwise. This won't affect PE and helps W10
-	//Crafting events and container slot changes help us decide whether the client is a desktop client or not.
-	//Okay, apparently this causes PE to lose stuff put into chests until they craft something. Argh!
-	protected $isDesktopClient = true;
 
 	private $loaderId = null;
 
@@ -818,10 +813,6 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 	 */
 	public function getPort() : int{
 		return $this->port;
-	}
-
-	public function isDesktop(): bool{
-		return $this->isDesktopClient;
 	}
 
 	public function getNextPosition(){
@@ -3313,6 +3304,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 				if($this->craftingInventory->contains($packet->item)){
 					//We're okay to go ahead and drop as Desktop GUI style
 					//We're dropping an item that we've clicked on and picked up.
+					echo "Dropped an item from the crafting inventory\n";
 					$droppedItem = $packet->item;
 					$this->craftingInventory->remove($droppedItem);
 					$replacementItem = null;					
@@ -3324,6 +3316,9 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 					if(!$heldItem->deepEquals($packet->item)){
 						//Should be impossible
 						//Player tried to drop something that wasn't the same as their held item
+						//STUPID: you can do this on PE. *facepalm*
+						echo "Player attempted to drop a wrong item\n";
+						$this->inventory->sendContents($this);
 						break;
 					}elseif($heldItem->getCount() !== $packet->item->getCount()){
 						//Player is trying to drop part of a slot
@@ -3331,13 +3326,17 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 						if($remaining < 0){
 							//Again, should be impossible
 							//Client trying to drop more of an item than they are holding
+							echo "Player attempted to drop more of an item than they have\n";
+							$this->inventory->sendContents($this);
 							break;
 						}else{
+							echo "Player dropping part of a held slot\n";
 							$heldItem->setCount($remaining);
 							$droppedItem = $packet->item;
 							$replacementItem = $heldItem;
 						}
 					}else{
+						echo "Dropping a whole held slot\n";
 						//Dropping the entire held slot
 						$droppedItem = $heldItem;
 					}
@@ -3451,10 +3450,10 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 
 				//Tells the server whether to add the resulting item to the inventory directly.
 				// We do not want to do this with Desktop GUI as this might result in duplication.
-				//$isDesktopCrafting = false;
+				$isDesktopCrafting = false;
 				
 				if(count($packet->input) === 0){
-					$this->isDesktopClient = true;
+					$isDesktopCrafting = true;
 					/* If the packet "input" field is empty this needs to be handled differently.
 					 * "input" is used to tell the server what items to remove from the client's inventory
 					 * Because crafting takes the materials in the crafting grid, nothing needs to be taken from the inventory
@@ -3490,7 +3489,6 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 						}
 					}
 				}else{
-					$this->isDesktopClient = false;
 					if($recipe instanceof ShapedRecipe){
 						for($x = 0; $x < 3 and $canCraft; ++$x){
 							for($y = 0; $y < 3; ++$y){
@@ -3581,7 +3579,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 					break;
 				}
 				//Only do this if the crafting was not desktop style
-				if(!$this->isDesktopClient){
+				if(!$isDesktopCrafting){
 					foreach($used as $slot => $count){
 						if($count === 0){
 							continue;
@@ -3712,14 +3710,7 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 				}
 
 				$this->currentTransaction->addTransaction($transaction);
-
-				if(count($this->currentTransaction->getTransactions()) > 1){
-					//Win10 can't make more than 1 inventory transaction at a time.
-					//If we get more than 1, this must be PE (adding items to a chest for example)
-					$this->isDesktopClient = false;
-				}
-				
-				if($this->currentTransaction->canExecute() or $this->isDesktopClient){
+				if($this->currentTransaction->canExecute()){
 					$achievements = [];
 					foreach($this->currentTransaction->getTransactions() as $ts){
 						$inv = $ts->getInventory();
@@ -3917,7 +3908,9 @@ class Player extends Human implements CommandSender, InventoryHolder, ChunkLoade
 	 * @param bool   $notify
 	 */
 	public final function close($message = "", $reason = "generic reason", $notify = true){
-
+		foreach($this->getCraftingInventory()->getContents() as $craftingItem){
+			$this->level->dropItem($this, $craftingItem);
+		}
 		if($this->connected and !$this->closed){
 			if($notify and strlen((string) $reason) > 0){
 				$pk = new DisconnectPacket;
